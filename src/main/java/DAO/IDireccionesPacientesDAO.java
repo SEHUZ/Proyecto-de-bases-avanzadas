28e8440f05d775dcac3e2d42ee604433ea2b79bc
SEHUZ <sonic15622@gmail.com>
2025-02-19 17:22:44 -0700

    Cambios en la capa de persistencia: Se ha creado el paquete de entidades donde van las clases para cada entidad, paciente, usuario, y direccionPaciente.
    Se crearon tambien los daos para todas las entidades y sus interfaces

--- src/main/java/DAO/IDireccionesPacientesDAO.java
+++ src/main/java/DAO/IDireccionesPacientesDAO.java
@@ -2,17 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package Conexion;
-
-import Exception.PersistenciaClinicaException;
-import java.sql.Connection;
+package DAO;
 
 /**
  *
  * @author sonic
  */
-public interface IConexionBD {
-    
-    public Connection crearConexion() throws PersistenciaClinicaException;
+public interface IDireccionesPacientesDAO {
     
 }
