0d3151bb04f82c28000356e2a7bcb6adbabeb6e9
DanielMR0509 <daniel.miramontes252801@potros.itson.edu.mx>
2025-02-19 22:29:34 -0700

    Agregados atributos a la clase Paciente. Agregado el metodo RegistrarPaciente en pacienteDAO. Correciones a PacienteBO. Agregado nuevamente el metodo a IPacienteDAO.

--- src/main/java/DAO/PacienteDAO.java
+++ src/main/java/DAO/PacienteDAO.java
@@ -5,6 +5,14 @@
 package DAO;
 
 import Conexion.IConexionBD;
+import Entidades.Paciente;
+import Exception.PersistenciaClinicaException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -20,5 +28,42 @@ public class PacienteDAO implements IPacienteDAO {
     }
 
     private static final Logger logger = Logger.getLogger(PacienteDAO.class.getName());
+
+    @Override
+    public Paciente registrarPaciente(Paciente paciente) throws PersistenciaClinicaException {
+
+        String sentenciaSQL = "INSERT INTO pacientes (idUsuario, nombres, apellidoPaterno, apellidoMaterno, fechaNacimiento, telefono) VALUES (?, ?, ?, ?, ?, ?)";
+        try (Connection con = conexion.crearConexion();
+                 PreparedStatement ps = con.prepareStatement(sentenciaSQL, Statement.RETURN_GENERATED_KEYS)) {
+            
+            ps.setInt(1, paciente.getIdUsuario());
+            ps.setString(2, paciente.getNombres());
+            ps.setString(3, paciente.getApellidoPaterno());
+            ps.setString(4, paciente.getApellidoMaterno());
+            ps.setObject(5, paciente.getFechaNacimiento());
+            ps.setString(6, paciente.getTelefono());
+            
+            int filasAfectadas = ps.executeUpdate();
+            if(filasAfectadas == 0) {
+                logger.severe("Falló el registro del paciente, ninguna fila ha sido afectada.");
+                throw new PersistenciaClinicaException("Falló el registro del paciente, ninguna fila ha sido afectada.");
+            }
+            
+            try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    paciente.setIdPaciente(generatedKeys.getInt(1));
+                    logger.info("Paciente registrado exitosamente. ID: " + paciente.getIdPaciente());
+                } else {
+                    logger.severe("Falló el registro del paciente, no se obtuvo ID.");
+                    throw new PersistenciaClinicaException("Falló el registro del paciente, no se obtuvo ID.");
+                }
+            }
+            
+            return paciente;
+        } catch (SQLException ex) {
+            logger.log(Level.SEVERE, "Error en el registro del paciente", ex);
+            throw new PersistenciaClinicaException("Error al registrar al paciente", ex);
+        }
+    }
 
 }
